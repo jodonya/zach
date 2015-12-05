9e37faad9d3e39cb5936f4228b39126c6db9005f
Japheth Odonya <jodonya@gmail.com>
2015-12-05 16:08:02 +0300

    initial commit of zach project

---
+++ src/main/java/com/zach/common/controller/GitOAuthController.java
@@ -0,0 +1,34 @@
+package com.zach.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.zach.model.UserLogin;
+
+@Controller
+@SessionAttributes("userLogin")
+@RequestMapping("/")
+//extends AbstractController 
+public class GitOAuthController {
+
+//	@Override
+//	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+//			HttpServletResponse response) throws Exception {
+//
+//		ModelAndView model = new ModelAndView("GitOAuthPage");
+//		model.addObject("msg", "Git Commits and Collaboration");
+//
+//		return model;
+//	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String main(ModelMap model) {
+		model.addAttribute(new UserLogin());
+		return "main";
+
+	}
+
+}
---
+++ src/main/java/com/zach/model/UserLogin.java
@@ -0,0 +1,16 @@
+package com.zach.model;
+
+public class UserLogin {
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
