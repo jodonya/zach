78fe205a6b06a0508b3c509ff3e465451226c1ef
Japheth Odonya <jodonya@gmail.com>
2015-12-06 11:48:45 +0300

    now able to read commits

--- src/main/java/com/zach/common/controller/OAuthMainController.java
+++ src/main/java/com/zach/common/controller/OAuthMainController.java
@@ -2,11 +2,12 @@ package com.zach.common.controller;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.kohsuke.github.GHBranch;
+import org.kohsuke.github.GHCommit;
 import org.kohsuke.github.GHOrganization;
+import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHUser;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.PagedIterable;
@@ -15,78 +16,89 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import com.zach.model.UserLogin;
 
 @Controller
-
-// extends AbstractController
+/***
+ * @author Japheth Odonya
+ * */
 public class OAuthMainController {
 
-//	@Override
-//	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-//			HttpServletResponse response) throws Exception {
-//		
-//		
-//		
-//		ModelAndView model = new ModelAndView("OAuthMainPage");
-//		model.addObject("msg", "The Commits");
-//		//model.addObject("userLogin", new UserLogin());
-//		
-//		UserLogin userLogin = (UserLogin)request.getAttribute("userLogin");
-//		
-//		
-//		System.out.println("##### Email is "+userLogin.getEmail());
-//
-//		return model;
-//	}
-	
+	public static String OAUTHACCESSTOKE = "026aa99777016a7499d3dc76d328b5e58d7213c9";
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String main(@ModelAttribute("userLogin") UserLogin userLogin, ModelMap model) {
-		//model.addAttribute(new UserLogin());
-		//return "main";
+	public String main(@ModelAttribute("userLogin") UserLogin userLogin,
+			ModelMap model) {
 		model.addAttribute("email", userLogin.getEmail());
-		System.out.println(" #### the email is "+userLogin.getEmail());
-		
-		//Connecting to Github using the GitHub API
-		GitHub github = null;;
+		System.out.println(" #### the email is " + userLogin.getEmail());
+
+		// Connecting to Github using the GitHub API
+		GitHub github = null;
 		try {
-			//github = GitHub.connectToEnterprise(apiUrl, oauthAccessToken);
-			github = GitHub.connectToEnterprise("", "");
+			github = GitHub.connectUsingOAuth(OAUTHACCESSTOKE);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		GHOrganization ghOrganization = null;
 		try {
 			ghOrganization = github.getOrganization("zachcollaboration");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+		// ghOrganization.getMembers()
+
+		// ghOrganization.get
+		// GHRe
+		Map<String, GHRepository> mapRepositories = null;
+		try {
+			mapRepositories = ghOrganization.getRepositories();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		int sequence = 0;
+		List<GHCommit> listCommits = null;;
+		for (Map.Entry<String, GHRepository> repository : mapRepositories
+				.entrySet()) {
+			listCommits = repository.getValue().listCommits().asList();
+			for (GHCommit ghCommit : listCommits) {
+				sequence++;
+				try {
+					System.out.println("Commit # "+sequence+" Author "+ghCommit.getAuthor()+" comment "+ghCommit.getCommitShortInfo()+" date "+ghCommit.getSHA1());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+			}
+
+		}
+
+		// ghOrganization.getRepositories();
+
 		PagedIterable<GHUser> listUsers = null;
 		try {
-			listUsers =ghOrganization.listMembers();
+			listUsers = ghOrganization.listMembers();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		System.out.println(" The number of members is "
+				+ listUsers.asList().size());
+
 		int count = 0;
-		for (GHUser ghUser : listUsers) {
+		for (GHUser ghUser : listUsers.asList()) {
 			count++;
-			try {
-				System.out.println("Member number "+count+" is "+ghUser.getEmail());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			// ghUser.get
+			System.out.println("Member number " + count + " is " + ghUser);
+
 		}
-		
-		
-		
+
 		return "OAuthMainPage";
 
 	}
